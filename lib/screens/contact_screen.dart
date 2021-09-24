44be15242db0ba26c7f2eafbd7338547edc6779d
Anh Pham <anhphamduy@outlook.com>
2021-09-24 22:28:48 +1000

    Finish adding contact and showing contacts

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dot_mobile/models/models.dart';
 import 'package:dot_mobile/widgets/authenticated_scaffold.dart';
+import 'package:empty_widget/empty_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'add_contact_screen.dart';
+import 'contact_details_screen.dart';
 
 class ContactScreen extends StatelessWidget {
   @override
@@ -62,43 +64,37 @@ class ContactScreen extends StatelessWidget {
                               backgroundColor:
                                   Get.theme.scaffoldBackgroundColor,
                               builder: (BuildContext context) {
-                                return Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 16.0,
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        width: 72,
-                                        height: 72,
-                                        child: CircleAvatar(
-                                          backgroundImage: NetworkImage(
-                                            e.data().avatar,
-                                          ),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(
-                                          vertical: 16.0,
-                                        ),
-                                        child: Text(
-                                          e.data().fullName,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: 18,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
+                                return ContactDetailsScreen(contact: e.data());
                               },
                               context: context,
                             );
                           },
                         );
                       }).toList();
+
+                      if (tiles.isEmpty) {
+                        return Container(
+                          margin: EdgeInsets.only(top: 64),
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(1000)),
+                            color: Colors.white,
+                          ),
+                          width: 240,
+                          height: 240,
+                          child: EmptyWidget(
+                            hideBackgroundAnimation: true,
+                            image: null,
+                            packageImage: PackageImage.Image_2,
+                            title: 'No contacts',
+                            titleTextStyle: TextStyle(
+                              fontSize: 22,
+                              color: Color(0xff9da9c7),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        );
+                      }
 
                       return Column(
                         children: ListTile.divideTiles(
